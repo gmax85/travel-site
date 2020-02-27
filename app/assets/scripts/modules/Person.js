840c512aeaf55d385a17d7a0145791b36a3d000a
Max <gerasimovpost@gmail.com>
2020-02-27 21:17:44 +0400

    Added header styles

---
+++ app/assets/scripts/modules/Person.js
@@ -0,0 +1,15 @@
+export class Person {
+  constructor(name, favoriteColor) {
+    this.name = name;
+    this.favoriteColor = favoriteColor;
+  }
+  greet() {
+    console.log(
+      " Hello, my name is " +
+        this.name +
+        " and my favorite color is " +
+        this.favoriteColor +
+        " . "
+    );
+  }
+}
